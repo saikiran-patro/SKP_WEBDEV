1b5b85db9cfbdc7c7a2a230191f7b21827d361ea
Sai Kiran Patro <saikiranpatro198@gmail.com>
2022-07-27 01:59:29 +0530

    optimised skills view

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './About.scss'
+import {FaYoutube} from "react-icons/fa";
 export default function About() {
   return (
     <div className='About' id='About'>
@@ -10,6 +11,10 @@ export default function About() {
               <h1>A li'l bit about myself</h1>
               <p>A technophile with good web development skills.Passionate about learning new technologies, and enjoy making tech content online on Youtube! <i class="fab fa-youtube"></i></p>
               <p>I'm interested in Information Technology, space technology inspired by Elon Musk and also huge marvel fan!😄 </p>
+              <h2>Check out my youtube channel!</h2>
+              
+               <iframe style={{"border-radius":"30px"}} width="300" height="200" src="https://www.youtube.com/embed/WTUULDLrf-k" title="Web 3.0: The next generation of web technology ! 🔥 🔥" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br /><br />
+              <button><a href="https://www.youtube.com/channel/UCPv9XuCE1Cho5QDYBcqWphA" rel="noreferrer" target="_blank"> <FaYoutube className="MenuIcon"/> Youtube!</a></button>
 
     
         </div>
--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -1,26 +1,29 @@
 import React from 'react'
 import './Skills.scss'
-import { DiAndroid ,DiHtml5,DiCss3,DiJavascript,DiPython, DiNodejs,DiMongodb,DiReact,DiGit,DiGithubBadge,DiWindows,DiLinux} from "react-icons/di";
+//import { DiAndroid ,DiHtml5,DiCss3,DiJavascript,DiPython, DiNodejs,DiMongodb,DiReact,DiGit,DiGithubBadge,DiWindows,DiLinux} from "react-icons/di";
 export default function Skills() {
-  return (
+  // <DiHtml5 className="skillIcon"/>
+  //       <DiCss3 className="skillIcon"/>
+  //       <DiJavascript className="skillIcon"/>
+  //       <DiPython className="skillIcon"/>
+  //       <DiNodejs className="skillIcon"/>
+  //       <DiMongodb className="skillIcon"/>
+  //       <DiReact className="skillIcon"/>
+  //       <DiGit className="skillIcon"/>
+  //       <DiGithubBadge className="skillIcon"/>
+  //       <DiAndroid className="skillIcon"/>
+  //       <DiWindows className="skillIcon"/>
+  //       <DiLinux className="skillIcon"/>
+   return (
     <div className='Skills' id='Skills'>
     <div className="skillContainer">
-        <DiHtml5 className="skillIcon"/>
-        <DiCss3 className="skillIcon"/>
-        <DiJavascript className="skillIcon"/>
-        <DiPython className="skillIcon"/>
-        <DiNodejs className="skillIcon"/>
-        <DiMongodb className="skillIcon"/>
-        <DiReact className="skillIcon"/>
-        <DiGit className="skillIcon"/>
-        <DiGithubBadge className="skillIcon"/>
-        <DiAndroid className="skillIcon"/>
-        <DiWindows className="skillIcon"/>
-        <DiLinux className="skillIcon"/>
+        <img src={require('./skills.png')} alt="skills" className="skillsImg"/>
+        <img src={require('./skills2.png')} alt="skills" className="skillsImg"/> 
+        <img src={require('./skills3.png')} alt="skills" className="skillsImg"/> 
 
         
     </div>
     
     </div>
-  )
+   )
 }
