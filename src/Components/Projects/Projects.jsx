5b3e22f7004fa05f963d3b719ec511b4a9714220
Sai Kiran Patro <saikiranpatro198@gmail.com>
2022-07-29 20:35:00 +0530

    Projects Section Optimized

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -54,13 +54,58 @@ export default function Projects() {
     
     </ul>
     <div className="projectsContainer">
-        <div className="Featuredproject">
+        <div className={"Featuredproject " +(Featuredtype && " active ") +(!Featuredtype && " deactive ")}>
             
             <div className="project">
                 <img src={require('./Assets/Featured/TeslaClone.png')}  alt="TeslaApp" />
                 <h3>Tesla App</h3>
                 <div className="buttonInAction">
-                     <button ><a href="#Intr8"><FaGithub />  View Code</a></button>
+                     <button ><a href="https://github.com/saikiran-patro/Tesla-APP" target="_blank" rel="noopener noreferrer"><FaGithub />  View Code</a></button>
+                 
+                </div>
+                             
+                
+
+            </div>
+            <div className="project">
+                <img src={require('./Assets/Featured/CloudPrinter.png')}  alt="CloudPrinter" />
+                <h3>Cloud Printer App</h3>
+                <div className="buttonInAction">
+                     <button ><a href="https://github.com/saikiran-patro/CloudPrinter" target="_blank" rel="noopener noreferrer"><FaGithub />  View Code</a></button>
+                 
+                </div>
+
+            </div>
+            <div className="project">
+                <img src={require('./Assets/Featured/WordBookExtension.png')}  alt="ChromeExtension" />
+                <h3>Word Book Extension</h3>
+                <div className="buttonInAction">
+                     <button ><a href="https://github.com/saikiran-patro/Word-Book" target="_blank" rel="noopener noreferrer"><FaGithub />  View Code</a></button>
+                     <button ><a href="https://chrome.google.com/webstore/detail/word-book/lacbcjnocgmecilghhgijlcfoeaoehbn?hl=en&authuser=0" target="_blank" rel="noopener noreferrer"> <FaPlus/> Add Extension</a></button>
+                 
+                </div>
+
+            </div>
+            <div className="project">
+                <img src={require('./Assets/Featured/FallDetection.png')}  alt="FallDetection" />
+                <h3>Human Fall Detection (Prototype)</h3>
+                <div className="buttonInAction">
+                     <button ><a href="https://github.com/saikiran-patro/Human-fall-detection-system" target="_blank" rel="noopener noreferrer"><FaGithub />  View Code</a></button>
+                     <button ><a href="https://www.youtube.com/watch?v=schAiYABB6k" target="_blank" rel="noopener noreferrer"> <FaYoutube/> Video</a></button>
+                 
+                </div>
+
+            </div>
+
+          
+        </div>
+        <div className={"Webproject "+(Webtype && " active ")+ (!Webtype && " deactive ")}>
+            
+            <div className="project">
+                <img src={require('./Assets/Web/CancerPrediction.png')}  alt="TeslaApp" />
+                <h3>Cancer Prediction App</h3>
+                <div className="buttonInAction">
+                     <button ><a href="https://github.com/saikiran-patro/CancerPrediction" target="_blank" rel="noopener noreferrer"><FaGithub />  View Code</a></button>
                  
                 </div>
                
@@ -72,37 +117,108 @@ export default function Projects() {
 
             </div>
             <div className="project">
-                <img src={require('./Assets/Featured/CloudPrinter.png')}  alt="CloudPrinter" />
+                <img src={require('./Assets/Web/CloudPrinter.png')}  alt="CloudPrinter" />
                 <h3>Cloud Printer App</h3>
                 <div className="buttonInAction">
-                     <button ><a href="#Intr8"><FaGithub />  View Code</a></button>
+                     <button ><a href="https://github.com/saikiran-patro/CloudPrinter" target="_blank" rel="noopener noreferrer"><FaGithub />  View Code</a></button>
                  
                 </div>
 
             </div>
             <div className="project">
-                <img src={require('./Assets/Featured/WordBookExtension.png')}  alt="ChromeExtension" />
-                <h3>Word Book Extension</h3>
+                <img src={require('./Assets/Web/ContactsApp.png')}  alt="ContactsApp" />
+                <h3>Contacts App</h3>
                 <div className="buttonInAction">
-                     <button ><a href="#Intr8"><FaGithub />  View Code</a></button>
-                     <button ><a href="#Intr8"> <FaPlus/> Add Extension</a></button>
+                     <button ><a href="https://github.com/saikiran-patro/Contact-Manager-App" target="_blank" rel="noopener noreferrer"><FaGithub />  View Code</a></button>
+                   
                  
                 </div>
 
             </div>
             <div className="project">
-                <img src={require('./Assets/Featured/FallDetection.png')}  alt="FallDetection" />
-                <h3>Human Fall Detection (Prototype)</h3>
+                <img src={require('./Assets/Web/Memorandum.png')}  alt="MemorandumApp" />
+                <h3>Memorandum App</h3>
                 <div className="buttonInAction">
-                     <button ><a href="#Intr8"><FaGithub />  View Code</a></button>
-                     <button ><a href="#Intr8"> <FaYoutube/> Video</a></button>
+                     <button ><a href="https://github.com/saikiran-patro/Memorandum_App" target="_blank" rel="noopener noreferrer"><FaGithub />  View Code</a></button>
+                  
                  
                 </div>
 
             </div>
+            
 
           
         </div>
+        <div className={"Mobileproject "+(Mobiletype && " active ")+ (!Mobiletype && " deactive ")}>
+            
+            <div className="project">
+                <img src={require('./Assets/Android/LexiconMaster.png')}  alt="LexiconMaster" />
+                <h3>Dictionary App</h3>
+                <div className="buttonInAction">
+                     <button ><a href="https://github.com/saikiran-patro/LexiconMaster" target="_blank" rel="noopener noreferrer"><FaGithub />  View Code</a></button>
+                 
+                </div>
+               
+                            
+                
+
+                
+                
+
+            </div>
+            <div className="project">
+                <img src={require('./Assets/Android/PrimeVideoStore.png')}  alt="PrimeVideoStore" />
+                <h3>Prime Video Store </h3>
+                <div className="buttonInAction">
+                     <button ><a href="https://github.com/saikiran-patro/PrimeVStore" target="_blank" rel="noopener noreferrer"><FaGithub />  View Code</a></button>
+                   
+                 
+                </div>
+
+            </div>
+            <div className="project">
+                <img src={require('./Assets/Android/TeslaClone.png')}  alt="TeslaClone" />
+                <h3>Tesla App</h3>
+                <div className="buttonInAction">
+                     <button ><a href="https://github.com/saikiran-patro/Tesla-APP" target="_blank" rel="noopener noreferrer"><FaGithub />  View Code</a></button>
+                  
+                 
+                </div>
+
+            </div>
+            
+
+          
+        </div>
+        <div className={"Iotproject "+(Iottype && " active ")+ (!Iottype && " deactive ")}>
+            
+        <div className="project">
+                <img src={require('./Assets/IoT/FallDetection.png')}  alt="FallDetection" />
+                <h3>Human Fall Detection (Prototype)</h3>
+                <div className="buttonInAction">
+                     <button ><a href="https://drive.google.com/file/d/1mStZAMkmx5BtHU50c9bGnzVOl4JHGm6e/view" target="_blank" rel="noopener noreferrer"><FaGithub />  View Code</a></button>
+                     <button ><a href="https://www.youtube.com/watch?v=schAiYABB6k" target="_blank" rel="noopener noreferrer"> <FaYoutube/> Video</a></button>
+                 
+                </div>
+
+        </div>
+        <div className="project">
+                <img src={require('./Assets/IoT/Home.png')}  alt="FallDetection" />
+                <h3>Voice Controlled Home Automation</h3>
+                <div className="buttonInAction">
+                     <button ><a href="https://github.com/saikiran-patro/Voice-controlled-Home-Automation" target="_blank" rel="noopener noreferrer"><FaGithub />  View Code</a></button>
+                     <button ><a href="https://www.youtube.com/watch?v=IoOA29knfkg" target="_blank" rel="noopener noreferrer"> <FaYoutube/> Video</a></button>
+                 
+                </div>
+
+            </div>
+          
+
+      
+    </div>
+
+    
+    
     
     
     
