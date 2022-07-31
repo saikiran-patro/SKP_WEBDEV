6dda76bd58b8b654e0ac7006e8240d6397c51502
Sai Kiran Patro <saikiranpatro198@gmail.com>
2022-08-01 02:57:48 +0530

    certifications part in progress

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import Projects from "./Components/Projects/Projects.jsx";
 import About from "./Components/About/About.jsx";
 import Contact from "./Components/Contact/Contact.jsx";
 import Intro from "./Components/Intro/Intro.jsx";
-import Certificate from "./Components/Certificates/Certificate.jsx"
 import Profiles from "./Components/Profiles/Profiles.jsx"
 import Skills from "./Components/Skills/Skills.jsx";
 import Menu from "./Components/Menu/Menu.jsx";
 import Navbar from "./Components/Navbar/Navbar.jsx";
+import Certificate from "./Components/Certificates/Certificate.jsx";
 import './App.scss'; 
 
 import {useState} from 'react';
@@ -27,8 +27,9 @@ function App() {
            <About/>
            <Skills/>
            <Projects/>
-           <Profiles/>
            <Certificate/>
+           <Profiles/>
+           
            <Contact/>
 
 
--- src/Components/Certificates/Certificate.jsx
+++ src/Components/Certificates/Certificate.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
-import './Certificate.scss'
+import React from "react";
+import "./Certificate.scss";
+import RN from "./Assets/RN.png";
+import Python from "./Assets/Python.png";
+
 export default function Certificate() {
+  
+
+  const myCertificate = [RN,Python]
+  
   return (
-    <div className="Certificates" id="Certificates"></div>
-  )
+    <div className="Certificates" id="Certificates">
+      <h1>CERTIFICATIONS</h1>
+    
+      
+      
+      
+      
+      
+      <div className="container">
+           <div className="certificateHolder">
+                <img  src={myCertificate[0]} alt="RN" />
+                
+          </div>
+
+          <img className="cert" src={require("./Assets/certi.png" )}alt="certi" />
+
+      </div>
+    </div>
+  
+
+  );
 }
